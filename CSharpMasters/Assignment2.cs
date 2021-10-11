2edf81fa9225a4d9e2a004769ca20e076e672a4d
Avi Perez <perezavidien@gmail.com>
2021-10-12 05:43:47 +0800

    simple yield usage

---
+++ CSharpMasters/Assignment2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpMasters
+{
+    public class Assignment2
+    {
+        // yield
+
+        public static void Run(int start, int end)
+        {
+            Console.WriteLine("Sequence numbers are:");
+
+            foreach (var item in GetList(start, end))
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.Read();
+        }
+
+        private static IEnumerable<int> GetList(int min, int max)
+        {
+            for (int i = 0; i < max; i++)
+            {
+                yield return min + 2 * i;
+            }
+        }
+    }
+}
---
+++ CSharpMasters/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CSharpMasters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a start number: ");
+            int.TryParse(Console.ReadLine(), out var start);
+
+            Console.WriteLine("Enter number of iterations: ");
+            int.TryParse(Console.ReadLine(), out var end);
+
+            Assignment2.Run(start, end);
+        }
+    }
+}
